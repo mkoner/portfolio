35183ad3505796ce77d4a9a9765152d3aa76c16e
mkoner <amzid@hotmail.fr>
2024-08-09 03:09:59 -0500

    Added projects component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Contact from './components/contact/Contact';
 import Skills from './components/skills/Skills'
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="App">
+      <Projects/>
       <Skills/>
       <Contact/>
     </div>
--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import './Projects.css';
+import projects from '../../data/projects';
+
+
+const ProjectCard = ({project}) => {
+    return (
+        <div className="project-card">
+                <h4> {project.name} </h4>
+                <p> {project.description} </p>
+                <a href={project.link} target="_blank" rel="noopener noreferrer">View on GitHub</a>
+        </div>
+    );
+}
+
+
 
 const Projects = () => {
     return (
         <div className="projects">
-            
+            <h2>Projects</h2>
+                {projects.map((project, index) => (
+                    <ProjectCard project={project} key={index}/>
+                ))}
         </div>
     );
 };
