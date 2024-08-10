074d627c5287c47d83871da79289b22f929ead2d
mkoner <amzid@hotmail.fr>
2024-08-10 00:26:13 -0500

    Added contact info

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Contact.css'; // Optional for styling
+import './Contact.css'; 
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Contact = () => {
@@ -7,15 +7,46 @@ const Contact = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Handle form submission logic here
-        console.log(`Email: ${email}, Message: ${message}`);
+        
+        const formData = {
+            name: name,
+            email: email,
+            message: message,
+            timestamp: new Date().toLocaleString(),
+        };
+    
+        try {
+            const response = await fetch('https://alaar9q8db.execute-api.us-east-1.amazonaws.com/v1/messages', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+    
+            if (response.ok) {
+                const result = await response.json();
+                console.log('Success:', result);
+                // Handle success (e.g., show a success message, clear the form, etc.)
+            } else {
+                console.error('Error:', response.statusText);
+                // Handle error (e.g., show an error message)
+            }
+        } catch (error) {
+            console.error('Request failed:', error);
+            // Handle request failure (e.g., show a network error message)
+        } finally{
+            setEmail("");
+            setMessage("");
+            setName("");
+        }
     };
+    
 
     return (
-        <div className="contact container-column">
-            <h2>Contact Me</h2>
+        <div className="contact container-row">
             <form onSubmit={handleSubmit} className='container-column'>
                 <div>
                     <label>Name:</label>
@@ -47,14 +78,22 @@ const Contact = () => {
             </form>
             
             {/* Social Links Section */}
-            <div className="social-links container-row">
-                <p>Connect with me:</p>
-                <a href="https://www.linkedin.com/in/your-profile" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin size={30} />
-                </a>
-                <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
-                    <FaGithub size={30} />
-                </a>
+            <div className="social-links container-column">
+                <h4>Mkoner</h4>
+                <p>
+                    Full stack Software Engineer
+                </p>
+                <p>
+                    m-koner@outlook.com
+                </p>
+                <div className="links container-row">
+                    <a href="https://www.linkedin.com/in/mamadou-kone-3b1694b9/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={30} />
+                    </a>
+                    <a href="https://github.com/mkoner" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={30} />
+                    </a>
+                </div>
             </div>
         </div>
     );
